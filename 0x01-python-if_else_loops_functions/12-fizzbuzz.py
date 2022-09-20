7af1f65d521434e3c428570c4b8db080a723f391
enoch13 <mwangienoch13@gmail.com>
2022-09-21 00:06:29 +0300

    Task 12 commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,5 +6,6 @@ def fizzbuzz():
         if num % 5 == 0:
             print("Buzz", end="")
         if num % 3 != 0 and num % 5 != 0:
+            print("FizzBuzz", end="")
+        else:
             print("{:d}".format(num), end="")
-            print(end=" ")
