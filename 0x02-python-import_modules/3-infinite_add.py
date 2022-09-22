31f186f9c1d86245f3f1028ba8c7f23a3e201409
enoch13 <mwangienoch13@gmail.com>
2022-09-22 16:23:33 +0300

    Task 3 commit

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    for n in sys.argv:
+        if n != sys.argv[0]:
+           sum += int(n)
+    print("{:d}".format(sum))
