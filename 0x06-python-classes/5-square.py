7ee7642e7ae5d3c8df4114ad761235836b42af73
enoch13 <mwangienoch13@gmail.com>
2022-10-12 23:24:18 +0300

    task commit

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" a class Square that defines a square by: (based on 4-square.py)
+"""
+
+
+class Square:
+    """ a class Square with instance attribute size and
+uses setters and getters to set and retrieve size int
+"""
+    def __init__(self, size=0):
+        """ define the size attribute"""
+        self.size = size
+
+    @property
+    def size(self):
+        """ retrieves and allows access to the size attribute """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Raises:
+        TypeError: if value is not an integer
+        ValueError: if value is not >= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """  that returns the current square area """
+        return self.__size ** 2
+
+    def my_print(self):
+        """ that prints in stdout the square with the character # """
+        for i in range(self.__size):
+            print("#" * self.__size)
+        if (self.__size == 0):
+            print("")
---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" a class Square that defines a square by: (based on 5-square.py) """
+
+
+class Square:
+    """ defines a square with size and position """
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        args: position and size
+        """
+        self.size = size
+        self.postion = position
+
+    @property
+    def size(self):
+        """ retrieves and allows acces of size attribute """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ sets the size attribute
+        args: value is to be an integer
+        Raises:
+         TypeError: if value is not an integer
+         ValueError: if value is not >= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """ retrieves and allows acces of position attribute """
+        return self.__position
+
+    @position.setter
+    def postion(self, value):
+        """
+        Raises:
+        TypeError: position must be a tuple of 2 positive integers
+        """
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not (isinstance(value[0], int) and isinstance(value[1], int)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not ((value[0] >= 0) and (value[1] >= 0)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """ returns the current square area """
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__position[1]):
+            print()
+        for j in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
